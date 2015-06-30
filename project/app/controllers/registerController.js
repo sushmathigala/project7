5cbfeacd602373030372be4871568b331fd274b1
sushmathigala <sushma572@gmail.com>
2015-06-29 22:03:01 -0700

    //code//

---
+++ project/app/controllers/registerController.js
@@ -0,0 +1,38 @@
+
+angular.module("myApp").controller("registerController"
+    ,function($scope) {
+
+
+            $scope.registerUrl = "app/templates/welcome.html";
+
+
+        
+
+        
+        /*console.log($scope.firstName);
+         console.log($scope.lastName);
+         console.log($scope.email);
+         console.log($scope.password);*/
+     /*   function init() {
+         //   $scope.registerPage="app/templates/register.html";
+            $scope.user = {
+                firstName: "",
+                lastName: "",
+                dob: "",
+                email: "",
+                password: ""
+            };
+            $scope.displayWelcomePage = false;
+
+            $scope.states = [
+                {"statecode": "AP", "name": "Andhra Pradesh", "capital": "vijayawada"},
+                {"statecode": "TS", "name": "Telangana", "capital": "Hyderabad"}
+            ];
+        }
+
+        init();*/
+    });
+
+
+
+
